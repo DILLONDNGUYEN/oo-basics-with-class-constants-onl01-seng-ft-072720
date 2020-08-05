ae52495cb00390ba146493cac2943855b1a7cfc4
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-05 08:22:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,19 +1,20 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
-  BRAND = []
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
   end
+
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  def initialize(brand)
-    @brand = brand	    
-    BRANDS << brand unless BRANDS.any? { |b| b == brand }
-  end	  
+
 end
